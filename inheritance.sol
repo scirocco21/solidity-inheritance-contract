045c6c2316f4419dc975c459647aa0f46a5e6788
Sebastian <sebgertz@gmail.com>
2019-04-03 12:05:21 -0400

    build: set up contract skeleton and define types

--- inheritance.sol
+++ inheritance.sol
@@ -1,1 +1,16 @@
-pragma solidity ^0.4.22;
+pragma solidity >=0.4.20;
+
+contract Inheritance {
+    address owner;
+    bool deceased;
+    uint money;
+
+    constructor() public payable {
+        owner = msg.sender;
+        money = msg.value;
+        deceased = false;
+    }
+
+    address[] wallets;
+    mapping (address => uint) inheritance;
+}
