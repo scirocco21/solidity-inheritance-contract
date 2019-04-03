87e25de99da0a932d5890263c3ebbafbc7305217
Sebastian <sebgertz@gmail.com>
2019-04-03 12:15:49 -0400

    build: add modifiers

--- inheritance.sol
+++ inheritance.sol
@@ -11,6 +11,18 @@ contract Inheritance {
         deceased = false;
     }
 
+    // check that conditions are met before contract is executed
+        // a) only run contract if there is an owner equal to owner of contract instance
+    modifier isOwner {
+        require (msg.sender == owner);
+        _;
+    }
+        // b) only run contract if contract owner is dead
+    modifier isDeceased {
+        require(deceased == true);
+        _;
+    }
+
     address[] wallets;
     mapping (address => uint) inheritance;
 }
